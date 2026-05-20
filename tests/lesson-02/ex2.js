49a5712ae2ee9b981b6113b056012c9599c9ab94
Cong Pham <phamduccong1234@gmail.com>
2026-05-20 13:23:01 +0700

    Add lesson 2 files demonstrating Git commands and JavaScript basics

---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,5 @@
+const myName = "Alex";
+const myName2 = "Nagi";
+console.log(myName);
+console.log(myName2);
+// Do const không thể gán lại giá trị mới
---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,5 @@
+const height = 77;
+const ok = height * 9 / 10;
+const max = height;
+const min = height * 8 / 10;
+console.log("Cân nặng lý tưởng của bạn là: " + ok + " kg," + " Cân nặng tối đa của bạn là: " + max + " kg," + " Cân nặng tối thiểu của bạn là: " + min + " kg");
