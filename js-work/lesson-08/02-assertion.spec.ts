8d051fd8587c2f44ede3bdf55baccc554c072e36
Cong Pham <phamduccong1234@gmail.com>
2026-06-04 22:36:35 +0700

    refactor: Improve code readability by formatting locator calls in tests

--- js-work/lesson-08/02-assertion.spec.ts
+++ js-work/lesson-08/02-assertion.spec.ts
@@ -13,7 +13,9 @@ test("Expect", async ({}) => {
 test("Material site - non web-first", async ({ page }) => {
   await page.goto("https://material.playwrightvn.com/019-enable-form.html");
   await page.waitForTimeout(10_000);
-  const isEnabled = await page.locator("//button[@id='submitButton']").isEnabled();
+  const isEnabled = await page
+    .locator("//button[@id='submitButton']")
+    .isEnabled();
   expect(isEnabled).toEqual(true);
 });
 
@@ -21,4 +23,16 @@ test("Material site - web-first", async ({ page }) => {
   await page.goto("https://material.playwrightvn.com/019-enable-form.html");
   const btnSubmit = page.locator("//button[@id='submitButton']");
   await expect(btnSubmit).toBeEnabled({ timeout: 10_000 });
+});
+
+test("Material site - demo to have class", async ({ page }) => {
+  await page.goto(
+    "https://material.playwrightvn.com/01-xpath-register-page.html",
+  );
+  await expect(page).toHaveURL(
+    "https://material.playwrightvn.com/01-xpath-register-page.html",
+  );
+
+  const containerLocator = page.locator("//div[@id='ancestor']");
+  await expect(containerLocator).toHaveClass("container", { timeout: 2_000 });
 });
