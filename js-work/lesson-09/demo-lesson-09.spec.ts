1b3e4d73025881fc5fc762baae8b5823805a13be
Cong Pham <phamduccong1234@gmail.com>
2026-06-08 22:47:32 +0700

    refactor: Enhance Playwright test structure and improve readability

--- js-work/lesson-09/demo-lesson-09.spec.ts
+++ js-work/lesson-09/demo-lesson-09.spec.ts
@@ -1,10 +1,30 @@
-import { test } from '@playwright/test';
+import { test } from "@playwright/test";
 
-test('Playwright selector', async ({ page}) => {
-    const title = await page.locator("//h1[@id='self']").textContent();
-    const title2 = await page.getByRole('heading', { name: 'User Registration' }).textContent();
+test("Playwright selector", async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/index.html");
+  await page
+    .getByRole("link", { name: "Bài học 1: Register Page (có đủ các element)" })
+    .click();
+  const title = await page.locator("//h1[@id='self']").textContent();
+  const title2 = await page
+    .getByRole("heading", { name: "User Registration" })
+    .textContent();
 
-    await page.getByRole("checkbox", {name: "Traveling"}).check();
-    await page.getByRole("checkbox", {name: "Cooking"}).check();
-    await page.getByRole("radio", { name: "Male" , exact: true}).click();
+  await page.getByRole("checkbox", { name: "Traveling" }).check();
+  await page.getByRole("checkbox", { name: "Cooking" }).check();
+  await page.getByRole("radio", { name: "Male", exact: true }).click();
+
+  await page.getByLabel("Username").fill("playwright");
+});
+
+test("Playwright selector 2", async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/index.html");
+  await page
+    .getByRole("link", { name: "Bài học 2: Thực hành DOM relation" })
+    .click();
+  const listItems = await page
+    .getByRole("listitem")
+    .filter({ hasText: "mì" })
+    .textContent();
+  console.log(listItems);
 });
